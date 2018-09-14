d089d081a29186713a0701ab36db8538f9cc6bc2
shikerby <shikerby@gmail.com>
2018-09-14 10:47:14 +0800

    add django2.0 auth views go on

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ How happy we are, To meet friends from afar!
 > Social networking sites that can be customized to publish information
 > FOR THINK DIFFERENT,VENI VIDI VICI
 > 可定制信息发布的社交网站
+我觉得你很有魅力, 你对我的主动性说明你对我有同感。尽管如此，传统的信任仍然需要我们在做爱之前进行数多次柏拉图式恋爱。我正在往这方面发展，但事实说来，我真正想做的只是尽快和你XXXX。
--- account/urls.py
+++ account/urls.py
@@ -1,17 +1,8 @@
-from django.urls import path
-from django.contrib.auth import views as auth_views
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     # path('login/', views.user_login, name='login'),
-    path('', views.dashboard, name='dashboard'),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    # path('logout-then-login/', logout_then_login, name='logout_then_login'),
-    # path('password-change/', PasswordChangeView.as_view(), name='password_change'),
-    # path('password-change/done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
-    # path('password-reset/', PasswordResetView.as_view(), name='password_reset'),
-    # path('password-reset-done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
-    # path('password-reset/confirm/', PasswordResetConfirmView.as_view(), 'password_reset_confirm'),
-    # path('password-reset/complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('', include('django.contrib.auth.urls')),
 ]
